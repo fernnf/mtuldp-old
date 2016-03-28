3da7535ba040014695b3d06362d708799012d71d
Fernando Farias <fernnf@gmail.com>
2016-03-28 20:54:52 -0300

    New class DAO

--- api/src/main/java/org/onosproject/mtuldp/api/storage/MtudpStoreDao.java
+++ api/src/main/java/org/onosproject/mtuldp/api/storage/MtudpStoreDao.java
@@ -20,7 +20,9 @@ import org.onosproject.mtuldp.api.link.MtuldpEdgeLink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -29,7 +31,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 /**
  * Created by fernando on 22/03/16.
  */
-public class MtudpStore {
+public class MtudpStoreDao {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -37,12 +39,12 @@ public class MtudpStore {
     private Map<String, MtuldpEdgeLink> edgeLinkStore;
 
 
-    public MtudpStore(){
-        this.directLinkStore = new ConcurrentHashMap<String, MtuldpDirectLink>();
-        this.edgeLinkStore = new ConcurrentHashMap<String, MtuldpEdgeLink>();
+    public MtudpStoreDao(){
+        this.directLinkStore = new ConcurrentHashMap<>();
+        this.edgeLinkStore = new ConcurrentHashMap<>();
     }
 
-    public void createLink(DefaultMtuldpLink link){
+    public boolean createLink(DefaultMtuldpLink link){
 
         checkNotNull(link, "The link cannot be null");
 
@@ -50,33 +52,78 @@ public class MtudpStore {
             MtuldpDirectLink tmpLink = (MtuldpDirectLink) link;
             if (!directLinkStore.containsValue(link)){
                 directLinkStore.put(tmpLink.getUrnLinkId(), tmpLink);
+                return true;
             } else {
                 log.warn("the direct link is already exist");
+                return false;
             }
 
         } else if (link instanceof MtuldpEdgeLink){
             MtuldpEdgeLink tmpLink = (MtuldpEdgeLink) link;
             if (!edgeLinkStore.containsValue(link)){
                 edgeLinkStore.put(tmpLink.getUrnId(),tmpLink);
+                return true;
             } else {
                 log.warn("the edge link is already exist");
+                return false;
             }
         } else {
             log.error("the link unknown");
         }
+        return false;
     }
 
-    public updateLink(String urn, DefaultMtuldpLink newlink){
+    public boolean updateLink(String urn, DefaultMtuldpLink newlink){
         checkNotNull(urn, "urn Cannot be null");
         checkNotNull(newlink, "mtudplink cannot be null");
 
         if (newlink instanceof MtuldpDirectLink) {
+            if (directLinkStore.get(urn).equals(newlink)){
+                log.debug("The new link object is equal to old object");
+                return false;
+            }
             directLinkStore.replace(urn, directLinkStore.get(urn), (MtuldpDirectLink) newlink);
+            log.debug("New Direct link inserted");
+            return true;
         } else if (newlink instanceof MtuldpEdgeLink){
+            if (edgeLinkStore.get(urn).equals(newlink)){
+                log.debug("The new link object is equal to old object");
+                return false;
+            }
+            edgeLinkStore.replace(urn, edgeLinkStore.get(urn), (MtuldpEdgeLink) newlink);
+            log.debug("New Edge link inserted");
+            return true;
 
+        } else {
+            log.error("the link unknown");
         }
-
-
+        return false;
     }
 
+    public MtuldpDirectLink getMtuldpDirectLink(String urn){
+        return directLinkStore.get(urn);
+    }
+
+    public MtuldpEdgeLink getMtuldpEdgeLink(String urn){
+        return edgeLinkStore.get(urn);
+    }
+
+    public Set<MtuldpDirectLink> getMtuldpDirectLinks(){
+        return (Set<MtuldpDirectLink>) directLinkStore.values();
+    }
+
+    public Set<MtuldpEdgeLink> getMtuldpEdgeLinks(){
+        return (Set<MtuldpEdgeLink>) edgeLinkStore.values();
+    }
+
+    public int getMtuLinkRate(String urn){
+        if (link instanceof MtuldpDirectLink){
+            return directLinkStore.get(urn).getMtuRate();
+        } else if (link instanceof MtuldpEdgeLink){
+            return edgeLinkStore.get(urn).getMtuRate();
+        }
+
+        log.error("The urn id unknown");
+        return -1;
+    }
 }
